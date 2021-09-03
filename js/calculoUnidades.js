6d4a685ea3a2584373d35924f53ee6178519edae
jherrera245 <245jh17@gmail.com>
2021-09-02 20:41:43 -0600

    Finalizando ejercicio de practica 1

--- js/calculoUnidades.js
+++ js/calculoUnidades.js
@@ -2,15 +2,47 @@
 
 function init() {
     //Ingresar datos a convertir
-    var operacion1 = prompt("Ingrese la cantidad de metros", "");
+    var operacion = prompt("Ingrese la cantidad de metros", "");
     var operaciones = document.getElementById("operaciones");
     var resultado = document.getElementById("resultado");
-    var contenido = "";
 
+    operaciones.innerHTML = generandoMenuOpciones();
+
+    var opciones = document.getElementsByTagName("a");
+
+    //Seleccionamos una operacion dada una opcion
+    for (let i = 0; i < opciones.length; i++) {
+        switch (i) {
+            case 0:
+                opciones[i].onclick = function () {
+                    resultado.innerHTML = operacion + " metros equivale a " + parseFloat(operacion * 3.28084).toFixed(2) + " pies";
+                }
+                break;
+            case 1:
+                opciones[i].onclick = function () {
+                    resultado.innerHTML = operacion + " metros equivale a " + parseFloat(operacion * 39.37008).toFixed(2) + " pulgadas";
+                }
+                break;
+            case 2:
+                opciones[i].onclick = function () {
+                    resultado.innerHTML = operacion + " metros equivale a " + parseFloat(operacion * 1.09361).toFixed(2) + " yardas";
+                }
+                break;
+            default:
+                resultado.innerHTML = "Ha ocurrido un error inesperado";
+                break;
+        }
+    }
+}
+
+window.onload = init();
+
+function generandoMenuOpciones() {
+    var contenido = "";
     contenido += "<header>\n";
     contenido += "\t<h1> Calcular </h1>\n";
     contenido += "</header>\n";
-    
+
     contenido += "<nav>\n";
     contenido += "\t<ul>\n";
     contenido += "\t\t<li>\n";
@@ -25,22 +57,5 @@ function init() {
     contenido += "\t</ul>\n";
     contenido += "</nav>\n";
 
-    operaciones.innerHTML = contenido;
-
-    var opciones = document.getElementsByTagName("a");
-
-    for (let i = 0; i < opciones.length; i++) {
-        switch (i) {
-            case 0:
-                opciones[i].onclick = function () {
-                    resultado.innerHTML = "";
-                }
-                break;
-        
-            default:
-                break;
-        }
-    }
+    return contenido;
 }
-
-window.onload = init();
